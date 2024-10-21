fd21bdecb8ecd3a7a964968ed4b507f7fda2318a
amir kasraeian <amirkasraeian611@gmail.com>
2024-10-21 10:29:55 +0330

    Creating description page for each course

--- src/pages/course/Course.jsx
+++ src/pages/course/Course.jsx
@@ -1,12 +1,56 @@
-import './Course.css'
-import MyNavbar from '../../components/navbar/Navbar';
+import { useParams } from "react-router-dom";
+import "./Course.css";
+import MyNavbar from "../../components/navbar/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { RxLapTimer } from "react-icons/rx";
+import { MdOutlineAttachMoney } from "react-icons/md";
 
 function Course() {
-    return(
-        <div>
-            <MyNavbar />
-            <h1>Course page</h1>
-        </div>
-    )
+  const courseId = useParams().courseId;
+  const [courseData, setCourseData] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/courses/${courseId}`)
+      .then((response) => setCourseData(response.data));
+  }, []);
+  return (
+    <div>
+      <MyNavbar />
+      <Container>
+        <Row className="row-cols-1 row-cols-md-2" style={{ marginTop: "70px" }}>
+          <Col>
+            <div className="courseCardContainer">
+              <div className="cardHeader">
+                <img src={courseData.img} alt="#" />
+              </div>
+              <div className="cardBody">
+                <p>
+                  <FaChalkboardTeacher size="20px" /> مدرس :{" "}
+                  <b>{courseData.writter}</b>
+                </p>
+                <p>
+                  <RxLapTimer size="20px" /> مدت زمان دوره :{" "}
+                  <b>{courseData.courseTime} ساعت</b>
+                </p>
+                <p>
+                  <MdOutlineAttachMoney size="20px" /> قیمت دوره :{" "}
+                  <b>{courseData.price} تومان</b>
+                </p>
+                <div className="cardFooter d-flex justify-content-start">
+                  <Button variant="outline-primary">خرید</Button>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col className="my-5">
+            <p style={{ textAlign: "justify" }}>{courseData.text}</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 export default Course;
